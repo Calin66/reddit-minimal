a4619bb53ff4ec9f9ec6769ea97197ebd5711d0c
Calin66 <62961099+Calin66@users.noreply.github.com>
2022-09-18 13:30:37 +0300

    Start of Reddit Minimal Project

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./navbar.css";
+import { BsReddit } from "react-icons/bs";
+
+export default function Navbar() {
+  return (
+    <div className="navbar-container">
+      <BsReddit color="gold" size={40}/>
+    </div>
+  );
+}
